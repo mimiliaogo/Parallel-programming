7d9ac2eda9d90f4259455101381cb652bc8620c3
mimi <mimiliao2000@gmail.com>
2019-07-26 16:37:07 +0800

    revise initial version of hw3b

--- hw3/hw3b.cc
+++ hw3/hw3b.cc
@@ -1,3 +1,4 @@
+/* hw3b initial version  */
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -8,9 +9,6 @@ void output(char* outFileName);
 
 void block_FW(int B);
 int ceil(int a, int b);
-// void cal(int B, int Round, int block_start_x, int block_start_y, int block_width, int block_height);
-
-__global__ void cal_kernal_phase1(int B, int Round, int block_start_x, int block_start_y,  int n, int *Dist_gpu);
 
 __global__ void cal_kernel(int B, int Round, int block_start_x, int block_start_y, int block_width, int block_height, int n, int* Dist_gpu);
 
@@ -33,7 +31,7 @@ void input(char* infile) {
     fread(&n, sizeof(int), 1, file);
     fread(&m, sizeof(int), 1, file);
 
-     cudaMallocHost(&Dist, sizeof(int)*n*n);
+    cudaMallocHost(&Dist, sizeof(int)*n*n);
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
             if (i == j) {
@@ -73,8 +71,6 @@ void block_FW(int B) {
     cudaMemcpy(Dist_gpu, Dist, sizeof(int)*n*n, cudaMemcpyHostToDevice);
 
 
-   // dim3 block(B, B);
-
     for (int r = 0; r < round; ++r) {
         printf("%d %d\n", r, round);
         fflush(stdout);
@@ -96,94 +92,27 @@ void block_FW(int B) {
     cudaMemcpy(Dist, Dist_gpu, sizeof(int)*n*n, cudaMemcpyDeviceToHost);
 }
 
-// void cal(
-//     int B, int Round, int block_start_x, int block_start_y, int block_width, int block_height) {
-//     int block_end_x = block_start_x + block_height;
-//     int block_end_y = block_start_y + block_width;
 
-//     for (int b_i = block_start_x; b_i < block_end_x; ++b_i) {
-//         for (int b_j = block_start_y; b_j < block_end_y; ++b_j) {
-//             // To calculate B*B elements in the block (b_i, b_j)
-//             // For each block, it need to compute B times
-//             for (int k = Round * B; k < (Round + 1) * B && k < n; ++k) {
-//                 // To calculate original index of elements in the block (b_i, b_j)
-//                 // For instance, original index of (0,0) in block (1,2) is (2,5) for V=6,B=2
-//                 int block_internal_start_x = b_i * B;
-//                 int block_internal_end_x = (b_i + 1) * B;
-//                 int block_internal_start_y = b_j * B;
-//                 int block_internal_end_y = (b_j + 1) * B;
-
-//                 if (block_internal_end_x > n) block_internal_end_x = n;
-//                 if (block_internal_end_y > n) block_internal_end_y = n;
-
-//                 for (int i = block_internal_start_x; i < block_internal_end_x; ++i) {
-//                     for (int j = block_internal_start_y; j < block_internal_end_y; ++j) {
-//                         if (Dist[i][k] + Dist[k][j] < Dist[i][j]) {
-//                             Dist[i][j] = Dist[i][k] + Dist[k][j];
-//                         }
-//                     }
-//                 }
-//             }
-//         }
-//     }
-// }
-
-
-// __global__ void cal_kernal_phase1(int B, int Round, int block_start_x, int block_start_y,  int n, int* Dist_gpu)
-// {
-
-//     //only one blocks
-//     int b_i = block_start_x ;
-//      int b_j = block_start_y ;
-
-//     int block_internal_start_x = b_i * B;
-//     int block_internal_start_y = b_j * B;
-
-//     for (int k = Round * B; k < (Round + 1) * B && k < n; ++k) {
-//         // To calculate original index of elements in the block (b_i, b_j)
-//         // For instance, original index of (0,0) in block (1,2) is (2,5) for V=6,B=2
-
-//         __syncthreads();
-
-
-//         int i = block_internal_start_x + threadIdx.x;
-//         int j = block_internal_start_y + threadIdx.y;
-//         if (i > n) i = n;
-//         if (j > n) j = n;
-
-
-//         if (Dist_gpu[i*n + k] + Dist_gpu[k*n + j] < Dist_gpu[i*n + j]) {
-//             Dist_gpu[i*n + j] = Dist_gpu[i*n + k] + Dist_gpu[k*n + j];
-//         }
-
-
-//     }
-// }
 
 //general kernel
 __global__ void cal_kernel(int B, int Round, int block_start_x, int block_start_y, int block_width, int block_height, int n, int* Dist_gpu) {
 
+        // from blockId get the block 2d position
         int b_i = block_start_x + blockIdx.x / block_width;
         int b_j = block_start_y + blockIdx.x % block_width;
 
-        int inner_round = (B*B-1)/blockDim.x + 1;
-
-
-
         for (int k = Round * B; k < (Round +1) * B && k < n; ++k) {
+        
+            int i = b_i * B + threadIdx.x  / B;
+            int j = b_j * B + threadIdx.x  % B;
 
-                for(int r=0; r<inner_round; r++){
+            if ((i>=n) | (j>=n)) continue ;
 
-                        int i = b_i * B + (threadIdx.x + r*blockDim.x) / B;
-                        int j = b_j * B + (threadIdx.x + r*blockDim.x) % B;
-
-                        if ((i>=n) | (j>=n)) continue ;
-
-                        if (Dist_gpu[i*n+k] + Dist_gpu[k*n+j] < Dist_gpu[i*n+j]) {
-                                Dist_gpu[i*n+j] = Dist_gpu[i*n+k] + Dist_gpu[k*n+j];
-                        }
-                }
-                __syncthreads();
+            if (Dist_gpu[i*n+k] + Dist_gpu[k*n+j] < Dist_gpu[i*n+j]) {
+                    Dist_gpu[i*n+j] = Dist_gpu[i*n+k] + Dist_gpu[k*n+j];
+            }
+            
+            __syncthreads();
         }
 
 }
